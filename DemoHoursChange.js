dd2c9d184601a8f34a26ffef0ffe5f2156bd69a2
Colin <colin5678@Hotmail.com>
2021-12-28 16:10:22 -0800

    Add files via upload

---
+++ DemoHoursChange.js
@@ -0,0 +1,23 @@
+
+
+
+function getChangeType(){
+	var x = document.querySelector('input[name="hourChangeType"]:checked').value;
+ 	 
+
+ 	 if (x == 1) {
+ 	 	document.getElementById("demo").innerHTML = x;
+
+ 	 }else if (x == 2) {
+ 	 	document.getElementById("demo").innerHTML = x;
+
+ 	 }else{
+ 	 	document.getElementById("demo").innerHTML = x;
+
+ 	 }
+
+ 	
+
+}
+
+
